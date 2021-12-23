4d63324e083bf0f71e489bc561f258588e2b003e
Dmitriy <47882198+kevinche75@users.noreply.github.com>
2021-12-23 22:43:43 +0300

    fix high prescale values

--- sound_driver.c
+++ sound_driver.c
@@ -1,10 +1,14 @@
-//
-// Created by ad3st on 30.11.2021.
-//
+/*
+ * sound_driver.c
+ *
+ *  Created on: Dec 23, 2021
+ *      Author: kevinche
+ */
 
 #include "sound_driver.h"
 #include "tim.h"
-#include "hal_driver.h"
+#include "main.h"
+#include <stdio.h>
 
 void sound_driver_init(void) {
     HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
@@ -12,8 +16,16 @@ void sound_driver_init(void) {
 };
 
 
-void sound_driver_set_frequency(uint16_t freq) {
-    TIM2->PSC = ((2 * HAL_RCC_GetPCLK1Freq()) / (2 * 10 * freq)) - 1;
+void sound_driver_set_frequency(uint32_t freq) {
+	char message[150] = "";
+	uint32_t psc = (((uint32_t)HAL_RCC_GetPCLK1Freq()) / (10 * freq)) - 1;
+	uint16_t arr = TIM2->ARR;
+	if (psc > 65635){
+		uint32_t psc = (((uint32_t)HAL_RCC_GetPCLK1Freq()) / (8 * 10 * freq)) - 1;
+		arr *= 8;
+	}
+    TIM2->PSC = (uint16_t)psc;
+    TIM2->ARR;
 };
 
 void sound_driver_volume_on() {
@@ -23,46 +35,3 @@ void sound_driver_volume_on() {
 void sound_driver_volume_mute() {
     TIM2->CCR1 = 0;
 };
-
-
-int melody_tick = 0;
-int new = 1;
-int played = 0;
-
-void sound_driver_tone(int melody, int duration) {
-    if (new) {
-        new = 0;
-        melody_tick = HAL_GetTick();
-    }
-    if (!new && HAL_GetTick() - melody_tick < duration){
-        if (HAL_GetTick() - melody_tick < duration* 0.9){
-            sound_driver_set_frequency(melody);
-            sound_driver_volume_on();
-            played = 1;
-            new = 1;
-        }
-        else
-            sound_driver_volume_mute();
-    }
-    else {
-        new = 1;
-        played = 0;
-        sound_driver_volume_mute();
-    }
-}
-
-
-void sound_driver_play(int cur_melody,int cur_pause, int tempo) {
-    int noteDuration = 0;
-    if (cur_pause > 0)
-        noteDuration = wholeNote(tempo) / cur_pause;
-    else if (cur_pause < 0) {
-        noteDuration = wholeNote(tempo) / abs(cur_pause);
-        noteDuration *= 1.5;
-    }
-    sound_driver_tone(cur_melody, noteDuration);
-}
-
-int wholeNote(int tempo) {
-    return (60000 * 4) / tempo;
-}
