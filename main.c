41eb7c82fddcfed6904e623e5ca5c60925ed3e05
Dmitrii Beliakov <kevinche75@gmail.com>
2021-12-24 17:08:17 +0300

    lab4

--- main.c
+++ main.c
@@ -4,22 +4,47 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * COPYRIGHT(c) 2019 STMicroelectronics
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
+#include "usart.h"
 #include "gpio.h"
+#include "oled.h"
+#include "fonts.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -33,13 +58,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define CLEAN 0
-#define RED GPIO_PIN_15
-#define YELLOW  GPIO_PIN_14
-#define GREEN GPIO_PIN_13
 
-#define  SET GPIO_PIN_SET
-#define  RESET GPIO_PIN_RESET
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -61,45 +80,32 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void switchRedYellow(int pin, int time) {
-    if (pin!= RED && pin!=YELLOW && pin!= CLEAN)
-        return;
-    HAL_GPIO_WritePin(GPIOD, RED, pin == RED ? SET:RESET);
-    HAL_GPIO_WritePin(GPIOD, YELLOW, pin == YELLOW? SET: RESET);
-    HAL_Delay(time);
-}
+typedef enum{
+	INPUT_PORT = 0x00,
+	OUTPUT_PORT = 0x01, 
+	POLARITY_INVERSION = 0x02,
+	CONFIG = 0x03 
+	}pca9538_regs_t;
 
+#define KB_ADDR 0xE2
 
-void switchGreen(int set, int time){
-    HAL_GPIO_WritePin(GPIOD,GREEN, set ? SET : RESET);
-    HAL_Delay(time);
-}
+	char scan_row(int row) {
+			char char_buff = 0;
+			HAL_StatusTypeDef status = HAL_I2C_Mem_Write(&hi2c1,KB_ADDR & 0xFFFE, OUTPUT_PORT, 1, &char_buff, 1,100);
 
+			if (status != HAL_OK)
+				return -1;
 
+			HAL_I2C_Mem_Write(&hi2c1,KB_ADDR, CONFIG, 1,&row, 1, 100);
 
-void blink(int pin, int time, int period){
-    if (pin!= RED && pin!=YELLOW && pin!= GREEN)
-        return;
-//    for (int i = period; i <= time; i+=period) {
-        HAL_GPIO_TogglePin(GPIOD, pin);
-//        HAL_Delay(period);
-//    }
-}
+			char col_buff = 0;
 
-void waitForPressBtn(int time){
-    for (int i = 0;i<=time;i+=50) {
-        HAL_Delay(50);
-        if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == GPIO_PIN_RESET){
-        	if (i <= time * 3 / 4) HAL_Delay(time/4);
-        	else
-        		HAL_Delay(time - i);
-        	return;
-        }
-    }
-}
+			status = HAL_I2C_Mem_Read(&hi2c1, KB_ADDR | 1, INPUT_PORT, 1,&col_buff, 1,100);
 
-long getCurrentTime(){
-    return HAL_GetTick();
+			if (status != HAL_OK)
+				return -1;
+
+return col_buff;
 }
 /* USER CODE END 0 */
 
@@ -111,6 +117,13 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 
+	long start = 0;
+	char buf[20] = "";
+	int stop = 0;
+	long delay_from_pause = 0;
+	long prevent_from_drebezg = 0;
+	long prevent_from_drebezg_stop = 0;
+	long prevent_delay=500;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -119,7 +132,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  long time;
+
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -131,49 +144,60 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  oled_Init();
+  start = HAL_GetTick();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-//      switchGreen(1, 5000);
-	  switchGreen(1, 0);
-      time = getCurrentTime();
-      while (getCurrentTime() - time < 5000){
-
-      }
-      time = getCurrentTime();
-      while (getCurrentTime() - time < 5000){
-          if (time % 500 == 0){
-              blink(GREEN, 0, 0);
-          }
-      }
-//	  blink(GREEN, 5000, 500);
-	  switchGreen(0, 0);
-//	  switchRedYellow(YELLOW, 5000);
-      switchRedYellow(YELLOW, 0);
-      time = getCurrentTime();
-      while (getCurrentTime() - time < 5000){
-
-      }
-	  switchRedYellow(RED, 0);
-//      waitForPressBtn(10000);
-      time = getCurrentTime();
-      int pressed = 0;
-      while (getCurrentTime() - time < 10000){
-              if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == GPIO_PIN_RESET){
-                  pressed = 1;
-              }
-              if (pressed){
-                  if (getCurrentTime() - time >= 7500)
-                      break;
-              }
-      }
-	  switchRedYellow(CLEAN, 0);
     /* USER CODE END WHILE */
+
+	  long checktime  = HAL_GetTick();
+	  if (prevent_from_drebezg && checktime-prevent_from_drebezg>prevent_delay)
+			  prevent_from_drebezg = 0;
+	  if (prevent_from_drebezg_stop && checktime-prevent_from_drebezg_stop>prevent_delay)
+	  			  prevent_from_drebezg_stop = 0;
+
+
+	if (!stop){
+	  buf[0] = '\0';
+	  long i = HAL_GetTick();
+	  long time = i-start - delay_from_pause;
+	  sprintf(buf,"%d:%d:%d.%d", time/3600000,(time/60000)%60,(time/1000)%60,(time/100)%10);
+	  oled_Fill(Black);
+	  oled_SetCursor(0,0);
+	  oled_WriteString(buf,Font_11x18,White);
+	  oled_UpdateScreen();
+	 }
+	int col = scan_row(0xFF ^ (1 << 0)) >> 4; // get only columns
+		if (((col >> 0) & 1) == 0 &&!prevent_from_drebezg) {
+				start = HAL_GetTick();
+				delay_from_pause = 0;
+				buf[0] = '\0';
+				sprintf(buf,"0:0:0.0");
+				 oled_Fill(Black);
+				oled_SetCursor(0,0);
+				oled_WriteString(buf,Font_11x18,White);
+				oled_UpdateScreen();
+				prevent_from_drebezg = HAL_GetTick(); 
+  }
+
+	if (((col >> 1) & 1) == 0 &&!prevent_from_drebezg_stop) {
+		if (!stop) {
+			stop = 1;
+			delay_from_pause = delay_from_pause - HAL_GetTick();
+		} else {
+			stop = 0;
+			if (delay_from_pause)
+				delay_from_pause = delay_from_pause + HAL_GetTick();
+		}
+		prevent_from_drebezg_stop = HAL_GetTick();
+	}
 
     /* USER CODE BEGIN 3 */
   }
@@ -189,34 +213,39 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
+  /**Configure the main internal regulator output voltage 
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /**Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 120;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB buses clocks
+  /**Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
+  HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_HSI, RCC_MCODIV_1);
 }
 
 /* USER CODE BEGIN 4 */
@@ -231,10 +260,7 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -247,10 +273,10 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{
+{ 
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
